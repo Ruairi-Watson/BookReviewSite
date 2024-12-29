61c72c62fa5885fae43af587298488c401d91113
Ubuntu <ubuntu@ip-172-31-86-87.ec2.internal>
2024-12-29 22:53:13 +0000

    Add new files to Branch2

--- db/seeds.rb
+++
@@ -1,7 +0,0 @@
-Book.create([
-  { title: "Neuromancer", author: "William Gibson", description: "The quintessential cyberpunk novel." },
-  { title: "Dune", author: "Frank Herbert", description: "A science fiction epic exploring politics, religion, and ecology." },
-  { title: "Snow Crash", author: "Neal Stephenson", description: "A mind-bending look at a virtual reality metaverse." },
-  { title: "The Hobbit", author: "J.R.R. Tolkien", description: "A fantasy adventure that sets the stage for Lord of the Rings." },
-  { title: "Foundation", author: "Isaac Asimov", description: "The rise and fall of civilizations in a distant future." }
-])
